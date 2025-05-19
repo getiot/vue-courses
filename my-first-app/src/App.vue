5716de3af6a5a0a064f891438d1b1921fc0df13b
luhuadong <luhuadong@163.com>
2025-05-19 20:29:50 +0800

    add my first component

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,47 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Hello from './components/Hello.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>你好，Vue！</h1>
+    <p>这是你的第一个 Vue 应用。</p>
+    <Hello />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container {
+  text-align: center;
+  margin-top: 100px;
 }
 </style>
---
+++ my-first-app/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <p>这是 Hello 组件！</p>
+  </div>
+</template>
+
+<script setup>
+// 目前没有逻辑
+</script>
+
+<style scoped>
+.hello {
+  color: #42b983;
+  font-weight: bold;
+  font-size: 20px;
+  border: 1px solid #42b983;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
